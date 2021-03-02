cf580522748c4ba2032b6779eed5368199c048bd
João <joaoluisok@gmail.com>
2021-03-02 02:39:58 -0300

    final v2

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 #include "../headers/graph.h"
 
-bool detectaCiclo_util(Grafo *G, int* visitados, int atual) {
+bool detectaCiclo_util(Grafo *G, bool* visitados, int atual) {
   if (visitados[atual])
     return true;
 
-  visitados[atual] = 1;
+  visitados[atual] = true;
   bool flag = false;
   for (int i = 0; i < G->n_vertices; ++i) {
     if (G->m_adjacencia[atual][i] == 1) {
@@ -21,8 +21,8 @@ bool detectaCiclo_util(Grafo *G, int* visitados, int atual) {
 }
 
 bool detectaCiclo(Grafo *G) {
-  int visitados[G->n_vertices];
-  int flag = 0;
+  bool visitados[G->n_vertices];
+  bool flag = false;
   for (int i = 0; i < G->n_vertices; ++i)
     visitados[i] = 0;
 
